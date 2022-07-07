bab629545d5efce698659150b047928bf06d11cc
jennerdulce <jennerdulce@gmail.com>
2022-07-06 17:23:51 -0700

    Completed Unprotected Index Trello Card

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,4 @@
 class Apartment < ApplicationRecord
+    validates :name, :street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :zipcode, presence: true
     belongs_to :user
 end
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets all the apartments from the database" do
+      apartment = Apartment.create {
+        name: 'Apartment Name',
+        street: '800 E Lexington Ave',
+        city: 'El Cajon',
+        zipcode: '92019',
+        state: 'California',
+        manager: 'Freddy Burger',
+        email: 'freddy@lexingtonpark.com',
+        price: '$2,090',
+        bedrooms: 2,
+        bathrooms: 2,
+        pets: 'yes',
+        image: 'https://images1.apartments.com/i2/ZNNLRXth5Ar5s3kTKbWzZC-Zdx4lWaOrDrS18n-OLLc/110/lexington-park-el-cajon-ca-primary-photo.jpg?p=1',
+        user_id: 1
+       }
+      get '/cats'
+      bentley = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(bentley.length).to eq 1
+    end
   end
 end
